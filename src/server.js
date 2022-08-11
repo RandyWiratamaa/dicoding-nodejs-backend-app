395666609675c248620caa4163fe1aa0e1f7e96f
tebetebe19 <randywiratama1@gmail.com>
2022-08-11 10:31:01 +0700

    Fundamental nodeJS - Dicoding

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const Hapi = require('@hapi/hapi');
-const routes = require('./api/notes/routes');
+const notes = require('./api/notes');
+const NotesServices = require('./services/inMemory/NotesServices');
 
 const init = async () => {
+    const notesService = new NotesServices();
     const server = Hapi.server({
         port:5000,
         host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
@@ -12,7 +14,12 @@ const init = async () => {
         },
     });
 
-    server.route(routes);
+    await server.register({
+        plugin: notes,
+        option: {
+            service: notesService,
+        }
+    })
 
     await server.start();
     console.log(`Server berjalan pada ${server.info.uri}`);
